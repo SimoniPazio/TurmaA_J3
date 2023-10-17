ad0a830c25bfbd41a4050cfef57f07d354a99b27
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-10-17 19:46:05 -0300

    teste

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ function tocaSomRisada(){
     document.querySelector("#som_tecla_risada").play();
     
 }
+const listadeTeclas = document.querySelectorAll(".tecla");
+
+listadeTeclas[2].onclick = tocaSomRisada;
+
