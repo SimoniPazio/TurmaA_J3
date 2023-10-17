1fbbb555a8895f18d22a03eb98eb4d67a988e75e
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-10-17 18:05:07 -0300

    .

--- main.js
+++ main.js
@@ -2,18 +2,3 @@ function tocaSomRisada(){
     document.querySelector("#som_tecla_risada").play();
     
 }
-const listadeTeclas = document.querySelectorAll(".tecla");
-
-listadeTeclas[2].onclick = tocaSomRisada;
-
-function tocaSomVaia(){
-    document.querySelector("#som_tecla_vaia").play();
-    
-}
-listadeTeclas[1].onclick = tocaSomVaia;
-
-function tocaSomErrado(){
-    document.querySelector("#som_tecla_errado").play();
-    
-}
-listadeTeclas[3].onclick = tocaSomErrado;
