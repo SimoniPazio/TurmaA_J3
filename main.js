0363b3ce26e8d315cb73ebb0ff82950221607a88
Simoni da Costa Pazio <127344406+SimoniPazio@users.noreply.github.com>
2023-10-24 19:13:21 -0300

    arrumado

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-function tocaSomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+function tocaSomAplausos(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
     
 }
 const listadeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
-while(contador < 9){
-    listadeTeclas[contador].onclick = tocaSomAplausos;
+while(contador < listadeTeclas.length){
+    const efeito = listadeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listadeTeclas[contador].onclick = Function(){
+        tocaSom(idAudio)
+    }
+    tocaSom("som_tecla_aplausos");
+
     contador = contador + 1;
     console.log(contador);
 }
