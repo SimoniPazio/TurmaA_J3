0952d5c95da5bc61b34d6e745697de7fbb19c5b3
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2023-10-24 18:44:50 -0300

    contador

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
+function tocaSomAplausos(){
+    document.querySelector("#som_tecla_aplausos").play();
     
 }
 const listadeTeclas = document.querySelectorAll(".tecla");
 
-listadeTeclas[2].onclick = tocaSomRisada;
+let contador = 0;
+while(contador < 8){
+    listadeTeclas[contador].onclick = tocaSomAplausos;
+    contador = contador + 1;
+    console.log(contador);
+}
+
 
